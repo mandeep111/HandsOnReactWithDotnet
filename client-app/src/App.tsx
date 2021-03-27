8468e209fae42801674ab200134b8157fa8805c6
mandeep_dhakal <mandiprajdhakal@bajratechnologies.com>
2021-03-27 10:40:42 -0500

    semantic ui header and list overview

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,37 +1,36 @@
-import React, {Component} from 'react';
-import logo from './logo.svg';
-import './App.css';
-import axios from 'axios';
+import React, { Component } from "react";
+import { Header, Icon, List } from "semantic-ui-react";
+import "./App.css";
+import axios from "axios";
 
 class App extends Component {
   state = {
-    values: []
-  }
+    values: [],
+  };
 
   componentDidMount() {
-    axios.get('http://localhost:5000/api/values')
-    .then((response) => {
+    axios.get("http://localhost:5000/api/values").then((response) => {
       this.setState({
-        values: response.data
-      })
-    })
-    
+        values: response.data,
+      });
+    });
   }
-  render () {
+  render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <ul>
-              {this.state.values.map((value: any) =>
-                <li key={value.id}>{value.name}</li>
-              )}
-          </ul>
-        </header>
+      <div>
+        <Header as="h2">
+          <Icon name="users" />
+          <Header.Content>Dotnet with React</Header.Content>
+        </Header>
+        <List>
+          {this.state.values.map((value: any) => (
+            <List.Item key={value.id}> {value.name}</List.Item>
+          ))}
+        </List>
+        <ul></ul>
       </div>
     );
   }
-  
 }
 
 export default App;
