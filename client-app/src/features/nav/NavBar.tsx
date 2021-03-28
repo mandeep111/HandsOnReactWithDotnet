6ad8dbc469204f3bb50ddf66576f0359a9c3a11c
mandeep_dhakal <mandiprajdhakal@bajratechnologies.com>
2021-03-28 03:50:50 -0500

    semantic navbar added

---
+++ client-app/src/features/nav/NavBar.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Button, Container, Menu } from "semantic-ui-react";
+
+export const NavBar = () => {
+  return (
+    <Menu fixed='top' inverted>
+     <Container>
+     <Menu.Item>
+       <img src='/assets/logo.png' />
+       Dotnet with React App
+     </Menu.Item>
+      <Menu.Item name="Activities" />
+      <Menu.Item>
+        <Button positive content='Create New Activity'></Button>
+      </Menu.Item>
+     </Container>
+    </Menu>
+  );
+};
