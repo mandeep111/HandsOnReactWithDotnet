7057e23f519f72190303844ea89cb598fc6405ff
mandeep_dhakal <mandiprajdhakal@bajratechnologies.com>
2021-03-31 10:16:59 -0500

    service layer added to handle request and response

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -4,6 +4,7 @@ import axios from "axios";
 import { IActivity } from "../models/Activity";
 import { NavBar } from "../../features/nav/NavBar";
 import { ActivityDashboard } from "../../features/activities/ActivityDashboard";
+import agents from "../api/agents";
 
 const App = () => {
   const [activities, setActivities] = useState<IActivity[]>([]);
@@ -42,11 +43,10 @@ const App = () => {
   }
 
   useEffect(() => {
-    axios
-      .get<IActivity[]>("http://localhost:5000/api/activities")
+    agents.Activities.list()
       .then((response) => {
         let activities: IActivity[] = [];
-        response.data.forEach(activity => {
+        response.forEach(activity => {
           activity.date = activity.date.split('.')[0];
           activities.push(activity);
         });
